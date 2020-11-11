6d45705964d113be06d2c6c2055fb681efd2d6d4
Giovanni Oliveira <giovanni-g@hotmail.com>
2020-11-11 18:33:13 -0300

    create db.json on index

--- src/store/index.js
+++ src/store/index.js
@@ -3,7 +3,7 @@ import Vuex from "vuex";
 import axios from "axios";
 
 Vue.use(Vuex);
-const api = "https://credere-frontend-test.netlify.app/db.json"
+const api = "http://localhost:3000"
 
 export default new Vuex.Store({
   state: {
